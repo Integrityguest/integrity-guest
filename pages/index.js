e8569071631648da0e4a8dbb2d9f6a98d2e88c46
Integrityguest <gerente.granjaparaiso@gmail.com>
2025-03-30 00:39:47 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Welcome to Integrity Guest</h1>
+      <p>This is the login and reporting platform for hosts.</p>
+    </div>
+  );
+}
